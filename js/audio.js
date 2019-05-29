6ad6cf06579aeae8f77d073a1328491d507c213d
Hojei Hsu(PC) <hhhhojeihsu@gmail.com>
2019-05-30 02:27:13 +0800

    Ability To Change Button Text

---
+++ js/audio.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function() {
+  $("#audio_btn").click(function () {
+    $(this).text("Restart");
+  });
+});
--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,13 @@
 'use strict';
-$("#inhale_sec").slider();
-$("#inhale_sec").on("slide", function(slideEvt) {
-  $("#inhale_sec_val").text(slideEvt.value);
-})
 
-$("#exhale_sec").slider();
-$("#exhale_sec").on("slide", function(slideEvt) {
-  $("#exhale_sec_val").text(slideEvt.value);
-})
+$(document).ready(function() {
+  $("#inhale_sec").slider();
+  $("#inhale_sec").on("slide", function(slideEvt) {
+    $("#inhale_sec_val").text(slideEvt.value);
+  })
+
+  $("#exhale_sec").slider();
+  $("#exhale_sec").on("slide", function(slideEvt) {
+    $("#exhale_sec_val").text(slideEvt.value);
+  })
+});
