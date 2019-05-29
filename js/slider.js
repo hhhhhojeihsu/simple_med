b58fca3a0028627a4bf9802bea467cffa3db9240
Hojei Hsu(PC) <hhhhojeihsu@gmail.com>
2019-05-30 02:16:06 +0800

    Add Website Prototype

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+'use strict';
+$("#inhale_sec").slider();
+$("#inhale_sec").on("slide", function(slideEvt) {
+  $("#inhale_sec_val").text(slideEvt.value);
+})
+
+$("#exhale_sec").slider();
+$("#exhale_sec").on("slide", function(slideEvt) {
+  $("#exhale_sec_val").text(slideEvt.value);
+})
