0583b88cdbaa0fa2d33627f99f837bc472966e76
Hojei Hsu(PC) <hhhhojeihsu@gmail.com>
2019-05-30 10:55:49 +0800

    Change Restart Button Color

--- js/audio.js
+++ js/audio.js
@@ -3,5 +3,7 @@
 $(document).ready(function() {
   $("#audio_btn").click(function () {
     $(this).text("Restart");
+    if(!$(this).hasClass("btn-danger"))
+      $(this).addClass("btn-danger");
   });
 });
